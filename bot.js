3567553cbb87a52ddff24eb41c6f5579dbbcee09
Martín <martinlufr@gmail.com>
2018-09-08 16:55:52 -0300

    Basic funtions & npm modules update

---
+++ bot.js
@@ -0,0 +1,51 @@
+//lurDiscordBot coded by lMartin3 using Discord.js
+//See license in LICENSE.txt
+console.log("Starting LDB...")
+
+//import libs
+const discord = require('discord.js');
+const colors = require("colors");
+
+//import json files
+const private = require('./data/private.json');
+const settings = require('./data/settings.json');
+const lang = require('./data/lang.json');
+
+//functions
+
+
+//set constants
+colors.setTheme({
+	silly: 'rainbow',
+	process: 'magenta',
+	verbose: 'cyan',
+	prompt: 'grey',
+	data: 'grey',
+	success: 'green',
+	info: 'cyan',
+	warn: 'yellow',
+	debug: 'blue',
+	error: 'red'
+});
+
+//init bot
+bot.login(botPrivate.token);
+
+
+
+
+//!====================================!
+//!             Listeners              !
+//!====================================!
+
+//Listener: Ready
+bot.on("ready", async () => {
+	console.log(colors.success(`${bot.user.username}` + " " + 'is ready'));
+	bot.generateInvite(["ADMINISTRATOR"]).then(
+		link => {
+			console.log(colors.info("Invite link= " + link));
+		}).catch(err => {
+			console.log(err.stack);
+		});
+
+});
